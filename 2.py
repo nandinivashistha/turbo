99bdaf2fade8c9103edce231144e7faf7d91c0c4
nandinivashistha <112176763+nandinivashistha@users.noreply.github.com>
2023-11-05 22:33:37 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,24 @@
+
+import math
+
+def nCr(n, r):
+    res = 1
+
+    # calculating nCr:
+    for i in range(r):
+        res = res * (n - i)
+        res = res // (i + 1)
+
+    return res
+
+def pascalTriangle(r, c):
+    element = nCr(r - 1, c - 1)
+    return element
+
+if __name__ == '__main__':
+    r = 5 # row number
+    c = 3 # col number
+    element = pascalTriangle(r, c)
+    print(f"The element at position (r,c) is: {element}")
+    
+        
