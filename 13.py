449ceebbd625d5017e425718c8fcd9a341f7269e
nandinivashistha <112176763+nandinivashistha@users.noreply.github.com>
2023-11-18 20:12:55 +0530

    13.py

---
+++ 13.py
@@ -0,0 +1,23 @@
+def searchMatrix(matrix, target):
+    n = len(matrix)
+    m = len(matrix[0])
+
+    # apply binary search:
+    low = 0
+    high = n * m - 1
+    while low <= high:
+        mid = (low + high) // 2
+        row = mid // m
+        col = mid % m
+        if matrix[row][col] == target:
+            return True
+        elif matrix[row][col] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return False
+
+matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+result = searchMatrix(matrix, 8)
+print("true" if result else "false")
+
