81b72a58ddacdc08c2e754d696f76a29d8c9272e
nandinivashistha <112176763+nandinivashistha@users.noreply.github.com>
2023-11-19 12:24:55 +0530

    trapping rainwater

---
+++ 40.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+
+
+def trap(arr: List[int]) -> int:
+    n = len(arr)
+    waterTrapped = 0
+    for i in range(n):
+        j = i
+        leftMax = 0
+        rightMax = 0
+        while j >= 0:
+            leftMax = max(leftMax, arr[j])
+            j -= 1
+        j = i
+        while j < n:
+            rightMax = max(rightMax, arr[j])
+            j += 1
+        waterTrapped += min(leftMax, rightMax) - arr[i]
+    return waterTrapped
+
+
+
+
+if __name__ == "__main__":
+    arr = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+    print(f"The water that can be trapped is {trap(arr)}")
